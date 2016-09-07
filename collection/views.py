3a3af20c72a0d98042969270b0df038c41c37f27
John Houghton <johnkirkhoughton1@gmail.com>
2016-09-07 12:45:37 -0700

    template tags completed

--- collection/views.py
+++ collection/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # create your new views here
 def index(request):
+    # defining the variable
+    number = 6
+    thing = "Super John"
     # this is your new view
-    return render(request, 'index.html')
+    return render(request, 'index.html',
+                  {'number' : number, 'thing':thing, }
+                  )
+
