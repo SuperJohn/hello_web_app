b72714a98dfa49db649ff7fa81218f802934a320
John Houghton <johnkirkhoughton1@gmail.com>
2016-09-06 19:23:04 -0700

    now have templates working

--- hello_web_app/urls.py
+++ hello_web_app/urls.py
@@ -13,12 +13,23 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import  admin
-
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+from django.views.generic import TemplateView
 from collection import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^$admin/', admin.site.urls),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^about/$',
+TemplateView.as_view(template_name='about.html'),
+        name='about'),
+
+    url(r'^contact/$',
+TemplateView.as_view(template_name='contact.html'),
+    name='contact'),
+
 ]
+
